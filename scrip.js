af17c2a2fb10ab16f0757741638c719b3d0cd131
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-07-20 11:10:22 +0500

    df

---
+++ scrip.js
@@ -0,0 +1,24 @@
+let button=document.querySelector('button')
+let h1 =document.querySelector('h1')
+
+button.addEventListener('click',()=>{
+let idinput=document.getElementById('id')
+let pasword=document.getElementById('parol')
+let inp=document.querySelector('input').value
+if(idinput.value==1 && pasword.value==2){
+    alert("parol togri" )
+}
+else(
+    alert("parol notogri" )
+       
+     
+)
+})
+
+let button2=document.querySelector('#btn')
+
+button2.addEventListener("click",()=>{
+    prompt("a")
+
+    
+})
